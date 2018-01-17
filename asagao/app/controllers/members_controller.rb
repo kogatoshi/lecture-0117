e41149c36d265492e7c79d489d211ad8d84c7b28
kogadoom <t.k0317@i.softbank.jp>
2018-01-17 03:10:58 +0000

    01/17 Rails 課題 6章

--- asagao/app/controllers/members_controller.rb
+++ asagao/app/controllers/members_controller.rb
@@ -22,11 +22,46 @@ class MembersController < ApplicationController
   end
 
   def create
+    # 会員の新規登録
+  def create
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to @member, notice: "会員を登録しました。"
+    else
+      render "new"
+    end
+  end
   end
 
   def update
+    # 会員情報の更新
+  def update
+    @member = Member.find(params[:id])
+    @member.assign_attributes(params[:member])
+    if @member.save
+      redirect_to @member, notice: "会員情報を更新しました。"
+    else
+      render "edit"
+    end
+  end
   end
 
   def destroy
+    # 会員の削除
+  def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    redirect_to :members, notice: "会員を削除しました。"
+  end
+  end
+  
+  # 新規作成フォーム
+  def new
+    @member = Member.new(birthday: Date.new(1980, 1, 1))
+  end
+
+  # 更新フォーム
+  def edit
+    @member = Member.find(params[:id])
   end
 end
